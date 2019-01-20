d5b1e788b575a94c46e28bd053dec7d97b0ca437
Eric Herman <eric@freesa.org>
2019-01-20 14:58:58 +0100

    LGPLv3+ boilerplate

--- 01-basics/blink.c
+++ 01-basics/blink.c
@@ -1,3 +1,12 @@
+/* blink.c: very basic firmware for an avr Arduino Mega2560
+ * Copyright (c) 2019 Eric Herman <eric@freesa.org>
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ */
+
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdint.h>
