f2363b23a951fb263f5233867a4f5208e182e268
Eric Herman <eric@freesa.org>
2019-01-20 14:42:53 +0100

    very basic blink example

---
+++ 01-basics/blink.c
@@ -0,0 +1,88 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdint.h>
+#include <stddef.h>
+
+/*****************************************************************/
+/* CONFIG params */
+/*****************/
+#ifndef Blink_delay_milliseconds
+#define Blink_delay_milliseconds 500
+#endif
+/*****************************************************************/
+
+/*****************************************************************/
+/* PLATFORM #defines                                             */
+/*                                                               */
+/* On my Arduino Mega2560 LED_BUILTIN is PB7 (pin 7 of PORTB)    */
+/*****************************************************************/
+#ifndef DDR_LED_BUILTIN
+#define DDR_LED_BUILTIN DDRB
+#endif
+
+#ifndef DD_LED_BUILTIN
+#define DD_LED_BUILTIN DDB7
+#endif
+
+#ifndef PB_LED_BUILTIN
+#define PB_LED_BUILTIN PORTB7
+#endif
+
+#ifndef PORT_LED_BUILTIN
+#define PORT_LED_BUILTIN PORTB
+#endif
+/*****************************************************************/
+
+/*****************************************************************/
+/* PROTOTYPES */
+/**************/
+static void set_bit_u8(volatile uint8_t *b, unsigned index, int val);
+/*****************************************************************/
+
+/*****************************************************************/
+/* PROGRAM */
+/***********/
+void setup(void)
+{
+	/* set LED_BUILTIN for output */
+	set_bit_u8(&DDR_LED_BUILTIN, DD_LED_BUILTIN, 1);
+}
+
+void loop(void)
+{
+	set_bit_u8(&PORT_LED_BUILTIN, PB_LED_BUILTIN, 1);
+	_delay_ms(Blink_delay_milliseconds);
+
+	set_bit_u8(&PORT_LED_BUILTIN, PB_LED_BUILTIN, 0);
+	_delay_ms(Blink_delay_milliseconds);
+}
+
+int main(void)
+{
+	setup();
+	for (;;) {
+		loop();
+	}
+}
+
+/*****************************************************************/
+
+/*****************************************************************/
+/* HELPER FUNCTIONS */
+/********************/
+
+/* let's avoid having to think carefully about bit twiddling syntax */
+static void set_bit_u8(volatile uint8_t *b, unsigned index, int val)
+{
+	/* More concise should work, but seems too tricky: */
+	/* val = val ? 1 : 0 */ ;
+	/* *b ^= (-val ^ *b) & (1U << offset); */
+	/* Instead, be a bit more verbose and trust the optimizer: */
+	if (val) {
+		*b |= (1U << index);
+	} else {
+		*b &= ~(1U << index);
+	}
+}
+
+/*****************************************************************/
